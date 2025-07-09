e5bcf4772bfebb5cbf22431587414e1e9bb6b1ad
iamleyardjp <kamangaleyard68@gmail.com>
2025-07-09 08:20:45 +0200

    New Changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -38,6 +39,8 @@ function Router() {
 }
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
