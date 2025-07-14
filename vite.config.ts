69cce64794518b434d6cf8a4eded27ddd1f0c199
Chrispine Mndala <78247340+Chrispine-1210@users.noreply.github.com>
2025-07-14 02:15:28 +0000

    new

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path, { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
+import path from 'node:path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'client', 'src'),
+      '@': path.resolve(__dirname, 'client/src'),
       '@shared': path.resolve(__dirname, 'shared'),
-      '@assets': path.resolve(__dirname, 'attached_assets'),
-    },
+      '@assets': path.resolve(__dirname, 'attached_assets')
+    }
   },
   root: path.resolve(__dirname, 'client'),
   build: {
     outDir: path.resolve(__dirname, 'dist/public'),
-    emptyOutDir: true,
+    emptyOutDir: true
   },
+  server: {
+    port: 5173,
+    open: true
+  }
 });
