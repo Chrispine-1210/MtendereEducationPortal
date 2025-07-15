90389b31a5a266e7f118ff1a8c006345f8572df4
Chrispine Mndala <78247340+Chrispine-1210@users.noreply.github.com>
2025-07-15 05:30:36 +0000

    New Changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { AuthProvider } from "@/hooks/use-auth";
