571e9e90021d8f7a4e9a4e79cd4621994765a664
Chrispine Mndala <78247340+Chrispine-1210@users.noreply.github.com>
2025-07-15 09:27:42 +0000

    new

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,4 +1,5 @@
 import type { Express } from "express";
+import { Request, Response } from "express";
 import { createServer, type Server } from "http";
 import { WebSocketServer, WebSocket } from "ws";
 import { storage } from "../../storage";
