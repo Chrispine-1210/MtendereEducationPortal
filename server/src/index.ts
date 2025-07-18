689706b601c4a1f9ab7fcee9f2ce5b672c583057
Chrispine Mndala <78247340+Chrispine-1210@users.noreply.github.com>
2025-07-18 04:11:11 +0000

    new

--- server/src/index.ts
+++ server/src/index.ts
@@ -12,75 +12,76 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   const start = Date.now();
-  const path = req.path;
-  let capturedJsonResponse: Record<string, any> | undefined = undefined;
+    const path = req.path;
+      let capturedJsonResponse: Record<string, any> | undefined = undefined;
 
-  const originalResJson = res.json;
-  res.json = function (bodyJson, ...args) {
-import session from "express-session";
-import passport from "passport";
-import routes from "./routes/routes";
-    capturedJsonResponse = bodyJson;
-    return originalResJson.apply(res, [bodyJson, ...args]);
-  };
+        const originalResJson = res.json;
+          res.json = function (bodyJson, ...args) {
+          import session from "express-session";
+          import passport from "passport";
+          import routes from "./routes/routes";
+              capturedJsonResponse = bodyJson;
+                  return originalResJson.apply(res, [bodyJson, ...args]);
+                    };
 
-app.use(session({ secret: "secret", resave: false, saveUninitialized: false }));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use("/api", routes);
-  res.on("finish", () => {
-    const duration = Date.now() - start;
-    if (path.startsWith("/api")) {
-      let logLine = `${req.method} ${path} ${res.statusCode} in ${duration}ms`;
-      if (capturedJsonResponse) {
-        logLine += ` :: ${JSON.stringify(capturedJsonResponse)}`;
-      }
+                    app.use(session({ secret: "secret", resave: false, saveUninitialized: false }));
+                    app.use(passport.initialize());
+                    app.use(passport.session());
+                    app.use("/api", routes);
+                      res.on("finish", () => {
+                          const duration = Date.now() - start;
+                              if (path.startsWith("/api")) {
+                                    let logLine = `${req.method} ${path} ${res.statusCode} in ${duration}ms`;
+                                          if (capturedJsonResponse) {
+                                                  logLine += ` :: ${JSON.stringify(capturedJsonResponse)}`;
+                                                        }
 
-      if (logLine.length > 80) {
-        logLine = logLine.slice(0, 79) + "…";
-      }
+                                                              if (logLine.length > 80) {
+                                                                      logLine = logLine.slice(0, 79) + "…";
+                                                                            }
 
-      log(logLine);
-    }
-  });
+                                                                                  log(logLine);
+                                                                                      }
+                                                                                        });
 
-  next();
-});
+                                                                                          next();
+                                                                                          });
 
-const server = http.createServer(app);
+                                                                                          const server = http.createServer(app);
 
-(async () => {
-  await registerRoutes(app);
+                                                                                          (async () => {
+                                                                                            await registerRoutes(app);
 
 
-  app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
-    const status = err.status || err.statusCode || 500;
-    const message = err.message || "Internal Server Error";
+                                                                                              app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+                                                                                                  const status = err.status || err.statusCode || 500;
+                                                                                                      const message = err.message || "Internal Server Error";
 
-    res.status(status).json({ message });
-    throw err;
-  });
+                                                                                                          res.status(status).json({ message });
+                                                                                                              throw err;
+                                                                                                                });
 
-  // importantly only setup vite in development and after
-  // setting up all the other routes so the catch-all route
-  // doesn't interfere with the other routes
-  if (app.get("env") === "development") {
-    await setupVite(app, server);
-  } else {
-    serveStatic(app);
-  }
+                                                                                                                  // importantly only setup vite in development and after
+                                                                                                                    // setting up all the other routes so the catch-all route
+                                                                                                                      // doesn't interfere with the other routes
+                                                                                                                        if (app.get("env") === "development") {
+                                                                                                                            await setupVite(app, server);
+                                                                                                                              } else {
+                                                                                                                                  serveStatic(app);
+                                                                                                                                    }
 
-  // ALWAYS serve the app on port 5000
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
-  const port = 5000;
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
-  });
+                                                                                                                                      // ALWAYS serve the app on port 5000
+                                                                                                                                        // this serves both the API and the client.
+                                                                                                                                          // It is the only port that is not firewalled.
+                                                                                                                                            const port = 5000;
+                                                                                                                                              server.listen({
+                                                                                                                                                  port,
+                                                                                                                                                      host: "0.0.0.0",
+                                                                                                                                                          reusePort: true,
+                                                                                                                                                            }, () => {
+                                                                                                                                                                log(`serving on port ${port}`);
+                                                                                                                                                                  });
 
-  initWebSocket(server);
-})();
+                                                                                                                                                                    initWebSocket(server);
+                                                                                                                                                                    })();
+                                                                                                                                                                    
