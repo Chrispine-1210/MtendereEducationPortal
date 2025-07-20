2b829f2f6e436be1a7991524927df7fdf6fa1572
Chrispine Mndala <78247340+Chrispine-1210@users.noreply.github.com>
2025-07-20 15:46:06 +0000

    new

---
+++ server/src/controllers/partnersController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+import { Request, Response } from 'express';
+
+export const getPartners = (req: Request, res: Response) => {
+  // TODO: Replace with actual partners logic
+  res.json({ message: 'Partners endpoint working!' });
+};
+export const getPartners = (req: Request, res: Response) => {
+  // TODO: Replace with actual partners logic
+  res.json({ message: 'Partners endpoint working!' });
+};
