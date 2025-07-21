ccf5b5d897e7876fcb3c52973f5d7ecd10b03c1c
Chrispine Mndala <78247340+Chrispine-1210@users.noreply.github.com>
2025-07-21 06:10:24 +0000

    jhj

--- server/src/vite.ts
+++ server/src/vite.ts
@@ -3,17 +3,12 @@ import type { Server } from "http";
 import type { ViteDevServer } from "vite";
 import path from "path";
 import {createServer as createServer} from "vite";
-import { error } from "console";
 
 let vite: ViteDevServer;
 
 export const setupVite = async (app: Express, server: Server) => {
   const viteModule = await import("vite");
   const createViteServer = viteModule.createServer || viteModule.createServer;
-
-  if (!createViteServer) {
-    throw new error ("createServer not found in vite Module import");
-  }
 
   vite = await createViteServer({
     server: { middlewareMode: true },
