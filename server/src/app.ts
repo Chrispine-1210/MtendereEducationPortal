d19b88d2788b4919c1d4b97d5974124c852c6795
Chrispine Mndala <78247340+Chrispine-1210@users.noreply.github.com>
2025-07-23 06:56:19 +0000

    new

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import testimonialRoutes from "./modules/testimonials/testimonials.routes";
+import userRoutes from "./modules/users/users.routes";
 
 dotenv.config();
 
@@ -12,5 +13,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/testimonials", testimonialRoutes);
+app.use("/api/users", userRoutes);
 
 export default app;
