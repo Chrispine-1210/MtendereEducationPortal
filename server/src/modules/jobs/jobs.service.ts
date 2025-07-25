e3a11fa2cc66008e1b828858beb8c97bbb32532a
Chrispine Mndala <78247340+Chrispine-1210@users.noreply.github.com>
2025-07-25 07:28:18 +0000

    new

--- server/src/modules/jobs/jobs.service.ts
+++ server/src/modules/jobs/jobs.service.ts
@@ -4,10 +4,15 @@ import { eq } from "drizzle-orm";
 
 export const getAllJobs = () => db.select().from(jobs);
 
-export const createJob = (data: any) => db.insert(jobs).values(data).returning();
+export const getJobById = (id: number) =>
+  db.select().from(jobs).where(eq(jobs.id, id)).then(r => r[0]);
+
+export const createJob = (data: any) =>
+  db.insert(jobs).values(data).returning();
 
 export const updateJob = (id: number, data: any) =>
   db.update(jobs).set(data).where(eq(jobs.id, id)).returning();
 
 export const deleteJob = (id: number) =>
   db.delete(jobs).where(eq(jobs.id, id)).returning();
+
