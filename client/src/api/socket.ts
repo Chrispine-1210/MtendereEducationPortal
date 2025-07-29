7d689f826367a599abe7595676c230aa8412fbb2
Chrispine Mndala <78247340+Chrispine-1210@users.noreply.github.com>
2025-07-29 15:44:25 +0000

    new

--- client/src/api/socket.ts
+++ client/src/api/socket.ts
@@ -2,5 +2,5 @@ import { Client } from '../../../node_modules/@types/pg';
 import { WebSocketClient } from './websocket';
 import { io } form "socket.io-Client";
 
-const socket socket = io(import.meta.env.VITE_SEVER_URL);
+const socket = io(import.meta.env.VITE_SEVER_URL);
 export default socket
