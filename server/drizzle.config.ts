b3a31bf89ffe171816a49c06293152fe31f87085
Chrispine Mndala <78247340+Chrispine-1210@users.noreply.github.com>
2025-07-31 06:39:11 +0000

    new

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  schema: "./", // path to your schema files
+  schema: "./src/schema/schema", // path to your schema files
   out: "./drizzle",             // migrations output folder
   driver: "pg",                 // or 'mysql2' / 'better-sqlite3'
   dbCredentials: {
