b1af39e788603da2c315f86b69cb62b6a036eb84
Chrispine Mndala <78247340+Chrispine-1210@users.noreply.github.com>
2025-08-08 11:24:08 +0000

    new

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { Config, defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
     out: './drizzle',
-    schema: './src/db/schema.ts',
+    schema: './shared/schema.ts',
     dialect: 'postgresql',
     dbCredentials: {
         url: process.env.DATABASE_URL!,
