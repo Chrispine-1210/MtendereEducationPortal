bbffe48f865a33b16da3726b5bbef02979f3314d
Chrispine Mndala <78247340+Chrispine-1210@users.noreply.github.com>
2025-08-08 08:34:26 +0000

    NEW

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "drizzle-kit";
+
+if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL, ensure the database is provisioned")
+}
+
+export default defineConfig({
+    out: "./drizzle",
+    schema: "./shared/schema.ts",
+    driver: "pg",
+    dialect: "postgresql",
+    dbCredentials: {
+        connectionString: process.env.DATABASE_URL!,
+    },
+})
