dbed8ec42c1adea34684e898271acaff598abc61
Chrispine Mndala <78247340+Chrispine-1210@users.noreply.github.com>
2025-08-08 14:10:36 +0000

    NEW

--- client/src/components/ui/alert.tsx
+++ client/src/components/ui/alert.tsx
@@ -1,6 +1,6 @@
 import { cva, type VariantProps } from "class-variance-authority";
 import React from "react";
-import { cn } from "@/lib/utils"ß;
+import { cn } from "@/lib/utils";
 
 const alertVariants = cva(
   "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
