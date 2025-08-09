95a91df52a4ec933a991296ab82d9fd77f997cfb
Chrispine Mndala <78247340+Chrispine-1210@users.noreply.github.com>
2025-08-09 09:10:11 +0000

    new

---
+++ server/drizzle.config.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import { Config, defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+    out: './drizzle',
+    schema: './shared/schema.ts',
+    dialect: 'postgresql',
+    dbCredentials: {
+        url: process.env.DATABASE_URL!,
+    },
+}) satisfies Config;
+
